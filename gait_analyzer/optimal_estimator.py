c67b988df9048e72841c65994bfc6470e7714f1b
Charbie <eve.charbonneau.1@umontreal.ca>
2025-02-13 14:52:56 +0400

    made the OCP smaller (less nodes, less DoFs) -> still NaNs in g_x:(

--- gait_analyzer/optimal_estimator.py
+++ gait_analyzer/optimal_estimator.py
@@ -99,9 +99,10 @@ class OptimalEstimator:
         self.qdot_opt = None
         self.tau_opt = None
         self.generate_contact_biomods()
-        self.prepare_reduced_experimental_data()
+        self.prepare_reduced_experimental_data(plot_exp_data_flag=True)
         self.prepare_ocp()
         self.solve()
+        self.save_optimal_reconstruction()
         if plot_solution_flag:
             self.solution.graphs(show_bounds=True, save_name=self.get_result_file_full_path(self.experimental_data.result_folder + "/figures"))
         if animate_solution_flag:
@@ -171,45 +172,108 @@ class OptimalEstimator:
             with open(original_model_path, "r+", encoding="utf-8") as file:
                 lines = file.readlines()
             with open(new_model_path, "w+", encoding="utf-8") as file:
-                for line in lines:
-                    file.write(line)
+                for i_line, line in enumerate(lines):
+                    if i_line + 1 in [558, 559, 560, 1014, 1015, 1016]:
+                        pass  # Remove the toes rotations
+                    elif i_line + 1 in [1586, 1587, 1588, 1654, 1655, 1656, 1914, 1915, 1916, 2417, 2418, 2419, 2485, 2486, 2487, 2745, 2746, 2747]:
+                        pass  # Remove the hands rotations
+                    else:
+                        file.write(line)
                 file.write(add_txt_per_condition(condition))
 
-    def prepare_reduced_experimental_data(self):
+    def prepare_reduced_experimental_data(self, plot_exp_data_flag: bool = False):
         """
         To reduce the optimization time, only one cycle is treated at a time
         (and the number of degrees of freedom is reduced?).
         """
         self.model_ocp = self.biorbd_model_path.replace(".bioMod", "_heelL_toesL.bioMod")
 
-        # Only the 10th right leg swing (while left leg in flat foot)
+        # Only one right leg swing (while left leg in flat foot)
         swing_timings = np.where(self.events.phases["heelL_toesL"])[0]
         right_swing_sequence = np.array_split(swing_timings, np.flatnonzero(np.diff(swing_timings) > 1) + 1)
         this_sequence_analogs = right_swing_sequence[self.cycle_to_analyze]
         this_sequence_markers = Operator.from_analog_frame_to_marker_frame(analogs_time_vector=self.experimental_data.analogs_time_vector,
                                                                             markers_time_vector=self.experimental_data.markers_time_vector,
                                                                             analog_idx=this_sequence_analogs)
-        marker_idx_range = range(this_sequence_markers[0], this_sequence_markers[-1])
-        self.n_shooting = len(list(marker_idx_range)) - 1
-        self.q_exp_ocp = self.q_filtered[:, list(marker_idx_range)]
-        self.qdot_exp_ocp = self.qdot[:, marker_idx_range]
-        self.tau_exp_ocp = self.tau[:, marker_idx_range]
+
+        # Skipping some frames to lighten the OCP
+        marker_start = this_sequence_markers[0]
+        marker_end = this_sequence_markers[-1]
+        marker_hop = 4
+        idx_to_keep = np.arange(marker_start, marker_end, marker_hop)
+
+        # Skipping some DoFs to lighten the OCP
+        dof_idx_to_keep = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 34, 35, 36, 37, 38])
+
+        self.n_shooting = idx_to_keep.shape[0] - 1
+        self.q_exp_ocp = self.q_filtered[np.ix_(dof_idx_to_keep, idx_to_keep)]
+        self.qdot_exp_ocp = self.qdot[np.ix_(dof_idx_to_keep, idx_to_keep)]
+        self.tau_exp_ocp = self.tau[np.ix_(dof_idx_to_keep, idx_to_keep)]
         self.f_ext_exp_ocp = {
             "left_leg": np.zeros((9, self.n_shooting+1)),
             "right_leg": np.zeros((9, self.n_shooting+1)),
         }
-        for i_frame, marker_frame in enumerate(marker_idx_range):
+        for i_frame, marker_frame in enumerate(idx_to_keep):
             idx_analogs = Operator.from_marker_frame_to_analog_frame(analogs_time_vector=self.experimental_data.analogs_time_vector,
                                                                     markers_time_vector=self.experimental_data.markers_time_vector,
-                                                                    marker_idx=marker_frame)
+                                                                    marker_idx=int(marker_frame))
             self.f_ext_exp_ocp["left_leg"][:, i_frame] = np.mean(
                 self.experimental_data.f_ext_sorted[0, :, idx_analogs-5:idx_analogs+5], axis=1
             )
             self.f_ext_exp_ocp["right_leg"][:, i_frame] = np.mean(
                 self.experimental_data.f_ext_sorted[1, :, idx_analogs-5:idx_analogs+5], axis=1
             )
-        self.markers_exp_ocp = self.experimental_data.markers_sorted_with_virtual[:, :, list(marker_idx_range)]
-        self.phase_time = self.n_shooting * self.experimental_data.markers_dt
+        self.markers_exp_ocp = self.experimental_data.markers_sorted_with_virtual[:, :, idx_to_keep]
+        self.phase_time = self.experimental_data.markers_time_vector[idx_to_keep[-1]] - self.experimental_data.markers_time_vector[idx_to_keep[0]]
+
+        if plot_exp_data_flag:
+            import matplotlib.pyplot as plt
+            time_plot = np.linspace(0, self.phase_time, self.n_shooting+1)
+
+            # Plot Q that are susceptible to gimbal lock
+            fig, axs = plt.subplots(5, 1)
+            for i_dof in range(3, 6):
+                axs[0].plot(time_plot, self.q_exp_ocp[i_dof, :], label=f"q_{i_dof}")
+            for i_dof in range(10, 13):
+                axs[1].plot(time_plot, self.q_exp_ocp[i_dof, :], label=f"q_{i_dof}")
+            for i_dof in range(17, 20):
+                axs[2].plot(time_plot, self.q_exp_ocp[i_dof, :], label=f"q_{i_dof}")
+            for i_dof in range(23, 26):
+                axs[3].plot(time_plot, self.q_exp_ocp[i_dof, :], label=f"q_{i_dof}")
+            for i_dof in range(29, 32):
+                axs[4].plot(time_plot, self.q_exp_ocp[i_dof, :], label=f"q_{i_dof}")
+            for i in range(5):
+                axs[i].plot(np.array([time_plot[0], time_plot[-1]]), np.array([-np.pi/2, -np.pi/2]), '--k')
+                axs[i].plot(np.array([time_plot[0], time_plot[-1]]), np.array([np.pi/2, np.pi/2]), '--k')
+                axs[i].legend()
+            plt.savefig('Dofs_exp.png')
+            plt.show()
+            print(f"There are {np.sum(np.isnan(self.q_exp_ocp))} NaNs in Q.")
+            print(f"There are {np.sum(np.isnan(self.qdot_exp_ocp))} NaNs in Qdot.")
+
+            # Plot Tau to see if there are NaNs
+            plt.figure()
+            for i_dof in range(self.tau_exp_ocp.shape[0]):
+                plt.plot(time_plot, self.tau_exp_ocp[i_dof, :], '.', label=f"tau_{i_dof}")
+                nan_idx = np.isnan(self.tau_exp_ocp[i_dof, :])
+                plt.plot(time_plot[nan_idx], self.tau_exp_ocp[i_dof, nan_idx], 'ok')
+            plt.legend()
+            plt.savefig('Tau_exp.png')
+            plt.show()
+            print(f"There are {np.sum(np.isnan(self.tau_exp_ocp))} NaNs in Tau.")
+
+            # Plot Markers to see if some are missing
+            plt.figure()
+            for i_marker in range(self.markers_exp_ocp.shape[1]):
+                for i_coordinate in range(3):
+                    plt.plot(time_plot, self.markers_exp_ocp[i_coordinate, i_marker, :], '.', label=f"marker_{i_marker}")
+                    nan_idx = np.isnan(self.markers_exp_ocp[i_coordinate, i_marker, :])
+                    plt.plot(time_plot[nan_idx], self.markers_exp_ocp[i_coordinate, i_marker, nan_idx], 'ok')
+            plt.legend()
+            plt.savefig('Markers_exp.png')
+            plt.show()
+            print(f"There are {np.sum(np.isnan(self.markers_exp_ocp))} markers missing.")
+
 
 
     def prepare_ocp(self):
@@ -239,40 +303,41 @@ class OptimalEstimator:
         objective_functions.add(
             objective=bioptim.ObjectiveFcn.Lagrange.TRACK_STATE, key="qdot", node=bioptim.Node.ALL, weight=0.01, target=self.qdot_exp_ocp
         )
-        objective_functions.add(
-            objective=bioptim.ObjectiveFcn.Lagrange.TRACK_GROUND_REACTION_FORCES,
-            weight=0.01,
-            target=self.f_ext_exp_ocp["left_leg"][6:9, :-1],
-            contact_index=[0, 1, 2, 3, 4],
-        )
-        # TODO: Charbie -> track CoP ?
-        # TODO: Charbie -> constrain the initial ground contact velocity to be the preferential treadmill speed ?
+        # objective_functions.add(
+        #     objective=bioptim.ObjectiveFcn.Lagrange.TRACK_GROUND_REACTION_FORCES,
+        #     weight=0.01,
+        #     target=self.f_ext_exp_ocp["left_leg"][6:9, :-1],
+        #     contact_index=[0, 1, 2, 3, 4],
+        # )
+        # # TODO: Charbie -> track CoP ?
+        # # TODO: Charbie -> constrain the initial ground contact velocity to be the preferential treadmill speed ?
 
         constraints = bioptim.ConstraintList()
-        constraints.add(
-            bioptim.ConstraintFcn.TRACK_CONTACT_FORCES,  # Only pushing on the floor, no pulling (Z heel)
-            min_bound=0,
-            max_bound=np.inf,
-            node=bioptim.Node.ALL_SHOOTING,
-            contact_index=2,
-        )
-        constraints.add(
-            bioptim.ConstraintFcn.TRACK_CONTACT_FORCES,  # Only pushing on the floor, no pulling (Z LMFH1)
-            min_bound=0,
-            max_bound=np.inf,
-            node=bioptim.Node.ALL_SHOOTING,
-            contact_index=3,
-        )
-        constraints.add(
-            bioptim.ConstraintFcn.TRACK_CONTACT_FORCES,  # Only pushing on the floor, no pulling (Z LMFH5)
-            min_bound=0,
-            max_bound=np.inf,
-            node=bioptim.Node.ALL_SHOOTING,
-            contact_index=4,
-        )
+        # constraints.add(
+        #     bioptim.ConstraintFcn.TRACK_CONTACT_FORCES,  # Only pushing on the floor, no pulling (Z heel)
+        #     min_bound=0,
+        #     max_bound=np.inf,
+        #     node=bioptim.Node.ALL_SHOOTING,
+        #     contact_index=2,
+        # )
+        # constraints.add(
+        #     bioptim.ConstraintFcn.TRACK_CONTACT_FORCES,  # Only pushing on the floor, no pulling (Z LMFH1)
+        #     min_bound=0,
+        #     max_bound=np.inf,
+        #     node=bioptim.Node.ALL_SHOOTING,
+        #     contact_index=3,
+        # )
+        # constraints.add(
+        #     bioptim.ConstraintFcn.TRACK_CONTACT_FORCES,  # Only pushing on the floor, no pulling (Z LMFH5)
+        #     min_bound=0,
+        #     max_bound=np.inf,
+        #     node=bioptim.Node.ALL_SHOOTING,
+        #     contact_index=4,
+        # )
 
         dynamics = bioptim.DynamicsList()  # TODO: Charbie -> Change for muscles
-        dynamics.add(bioptim.DynamicsFcn.TORQUE_DRIVEN, with_contact=True)
+        dynamics.add(bioptim.DynamicsFcn.TORQUE_DRIVEN, with_contact=False, phase_dynamics=bioptim.PhaseDynamics.SHARED_DURING_THE_PHASE)
+        ##################################################################################################
 
         dof_mappings = bioptim.BiMappingList()
         dof_mappings.add(
@@ -283,8 +348,8 @@ class OptimalEstimator:
         x_bounds = bioptim.BoundsList()
         # TODO: Charbie -> Change for maximal range of motion during the trial to simulate limited RoM
         # TODO: Charbie -> Change for maximal velocity during the trial to simulate limited Power
-        x_bounds["q"] = bio_model.bounds_from_ranges("q")
-        x_bounds["qdot"] = bio_model.bounds_from_ranges("q")
+        # x_bounds["q"] = bio_model.bounds_from_ranges("q")
+        # x_bounds["qdot"] = bio_model.bounds_from_ranges("qdot")
 
         x_init = bioptim.InitialGuessList()
         x_init.add("q", initial_guess=self.q_exp_ocp, interpolation=bioptim.InterpolationType.EACH_FRAME)
@@ -317,11 +382,18 @@ class OptimalEstimator:
             # phase_transitions=phase_transitions,
             variable_mappings=dof_mappings,
             use_sx=False,
+            n_threads=10,
         )
 
     def solve(self, show_online_optim: bool = False):
         solver = bioptim.Solver.IPOPT(show_online_optim=show_online_optim)
+        solver.set_tol(1e-6)
         self.solution = self.ocp.solve(solver=solver)
+        self.time_opt = self.solution.decision_time(to_merge=bioptim.SolutionMerge.NODES, time_alignment=bioptim.TimeAlignment.STATES)
+        self.q_opt = self.solution.decision_states(to_merge=bioptim.SolutionMerge.NODES)["q"]
+        self.qdot_opt = self.solution.decision_states(to_merge=bioptim.SolutionMerge.NODES)["qdot"]
+        self.tau_opt = self.solution.decision_controls(to_merge=bioptim.SolutionMerge.NODES)["tau"]
+        self.opt_status = "CVG" if self.solution.status == 0 else "DVG"
 
     def extract_muscle_forces(self):
         # TODO: Charbie -> Extract muscle forces from the solution
@@ -331,7 +403,7 @@ class OptimalEstimator:
         if result_folder is None:
             result_folder = self.experimental_data.result_folder
         trial_name = self.experimental_data.c3d_file_name.split("/")[-1][:-4]
-        result_file_full_path = f"{result_folder}/optim_estim_{trial_name}.pkl"
+        result_file_full_path = f"{result_folder}/optim_estim_{trial_name}_{self.opt_status}.pkl"
         return result_file_full_path
 
     def save_optimal_reconstruction(self):
